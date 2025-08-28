8eac9ba632f24754769f6318e0750aa08bcae366
TinMH <tinhongmai1012@gmail.com>
2025-08-28 19:30:09 +0700

    Add register form

--- src/main/java/vn/host/dao/UserDao.java
+++ src/main/java/vn/host/dao/UserDao.java
@@ -4,5 +4,10 @@ import vn.host.model.User;
 
 public interface UserDao {
 	User get(String username);
+	void insert(User user);
+	boolean checkExistEmail(String email);
+	boolean checkExistUsername(String username);
+	boolean checkExistPhone(String phone);
+
 	
 }
